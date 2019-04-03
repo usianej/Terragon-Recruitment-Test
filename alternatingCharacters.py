6388132402561164132c4057f164998d9af46c23
Usianene Jefferson <49212502+usianej@users.noreply.github.com>
2019-04-03 16:54:42 +0100

    Update alternatingCharacters.py

--- alternatingCharacters.py
+++ alternatingCharacters.py
@@ -1,19 +1,15 @@
-def check_text(text):
-    text_splitted =[chr for chr in text]
-    count =0
-    redundant_chr_count=0
-
-    for chr in text_splitted:
-        try:
-            prev_chr= text_plitted[text_splitted.index(chr)-1]
-        except IndexError:
-            prev_chr = None
-
-            if prev_chr == chr:
-                del text_splitted[count]
-                redundant_chr_count+=1
-                count -=1
-                count +=1
-
-                return text_splitted.join(''),
-            redundant_chr_count
+Def character_Deletions(s)
+S= ['A','A','B','A','A','B']
+ If s.count(s[0])== Len(s)
+ return Len(s)-1
+ i =0
+ n= Len(s)
+ Pr = ' '
+ Count = 0
+ While (I < n) :
+    If s[1] == Pr:
+       Del s[1]
+       Count +=1
+     Pr = s[1]
+     I += 1
+return count

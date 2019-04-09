8c94113d55d79d370f1803fe1244dce9b417b606
Usianene Jefferson <49212502+usianej@users.noreply.github.com>
2019-04-09 14:32:23 +0100

    Found a better solution
    Decided to take a second look and saw a much easier way.

--- alternatingCharacters.py
+++ alternatingCharacters.py
@@ -1,15 +1,18 @@
-Def character_Deletions(s):
-S= ['A','A','B','A','A','B']
- If s.count(s[0])== Len(s)
- return Len(s)-1
- i =0
- n= Len(s)
- Pr = ' '
- Count = 0
- While (I < n) :
-    If s[1] == Pr:
-       Del s[1]
-       Count +=1
-     Pr = s[1]
-     I += 1
-return count
+prob = input ("please enter prob:")
+prob = list(prob)
+int (len(prob))
+count=0
+prv=0
+nxt=1
+
+while nxt < len(prob):
+  if prob[prv]==prob[nxt]:
+        del prob[nxt]
+        count=count+1
+  else:
+        prv=prv+1
+        nxt=nxt+1
+        
+print("A total of ", count, " item(s) have been deleted")
+print(prob)
+
